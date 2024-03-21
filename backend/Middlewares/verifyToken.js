03c957eaf179a789a31593ac012a8f4230d02afb
rajesh <rajesh.t7982@gmail.com>
2024-03-21 16:09:45 +0530

    v2

---
+++ backend/Middlewares/verifyToken.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  //get bearer token from headers opf req
+  let bearerToken = req.headers.authorization;
+  if (bearerToken === undefined) {
+    return res.send({ message: "Unauthorized access" });
+  }
+  let token = bearerToken.split(" ")[1];
+
+  try {
+    let decodedToken = jwt.verify(token, "abcdef");
+    next();
+  } catch (err) {
+    res.send({ message: err.message });
+  }
+};
+
+module.exports = verifyToken;
